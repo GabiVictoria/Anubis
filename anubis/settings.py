4b07d0620fce3378d81508306163c86744b75854
Gabrielle Victoria <gabrielebatista2810@gmail.com>
2025-07-14 03:04:25 -0300

    Corrige settings de produção para Railway

--- anubis/settings.py
+++ anubis/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = [
+    '.railway.app',
     'localhost',
     '127.0.0.1',
 ]
