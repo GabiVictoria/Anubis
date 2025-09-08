aa72c4597f822cd3302a18751ac0568b3d7b9d8d
Gabrielle Victoria <gabrielebatista2810@gmail.com>
2025-09-08 20:28:03 -0300

    teste

--- anubis/settings.py
+++ anubis/settings.py
@@ -25,6 +25,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # rodar local
 # DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
